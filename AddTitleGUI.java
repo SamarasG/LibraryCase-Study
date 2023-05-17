92671b6754bec2962fc0442fc1110d05e54f4121
SamarasG <gsamaras@york.citycollege.eu>
2023-05-17 10:07:18 +0300

    gui almost done

--- AddTitleGUI.java
+++ AddTitleGUI.java
@@ -22,7 +22,8 @@ public class AddTitleGUI extends JFrame {
 	private JLabel authorEdLb;
 
 	AddTitleGUI() {
-		this.setTitle("Add Tittle");
+		this.setTitle("Add 	New Tittle");
+		// the panel
 		contentPanel = (JPanel) this.getContentPane();
 		mainPanel = new JPanel(new GridLayout(3, 2));
 		buttonsPanel = new JPanel();
@@ -38,6 +39,19 @@ public class AddTitleGUI extends JFrame {
 		idLb = new JLabel("ID:");
 		titleLb = new JLabel("Title:");
 		authorEdLb = new JLabel("Author/Editor:");
+		// buttons container
+		contentPanel.add(buttonsPanel,BorderLayout.SOUTH);
+		buttonsPanel.add(addBookBt);
+		buttonsPanel.add(addJournalBt);
+		buttonsPanel.add(viewAllBt);
+		// fields container
+		contentPanel.add(mainPanel,BorderLayout.CENTER);
+		mainPanel.add(idLb);
+		mainPanel.add(IdtF);
+		mainPanel.add(titleLb);
+		mainPanel.add(titleTf);
+		mainPanel.add(authorEdLb);
+		mainPanel.add(authorEdTf);
 
 	}
 }
