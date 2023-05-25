9a664bcee1832c5f220eda3333d2f78e4234f747
SamarasG <gsamaras@york.citycollege.eu>
2023-05-25 17:26:59 +0300

    AvailabilityException

---
+++ LoanLimitException.java
@@ -0,0 +1,14 @@
+package LibraryCaseStudy;
+
+public class LoanLimitException extends Exception {
+
+	public LoanLimitException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoanLimitException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
