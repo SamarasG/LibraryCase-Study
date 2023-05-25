8dc4aedbaa6fa8e086cc4f52893895cb21168f12
SamarasG <gsamaras@york.citycollege.eu>
2023-05-25 16:24:34 +0300

    AvailabilityException

---
+++ AvailabilityException.java
@@ -0,0 +1,31 @@
+package LibraryCaseStudy;
+
+public class AvailabilityException extends Exception {
+
+	public AvailabilityException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AvailabilityException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AvailabilityException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AvailabilityException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AvailabilityException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
